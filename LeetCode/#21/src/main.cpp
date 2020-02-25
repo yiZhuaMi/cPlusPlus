e531219d8fea6b252ca1d158b73ffdb4f2b83574
yiZhuaMi <ttmarff@163.com>
2020-02-25 17:00:46 +0800

    complete #21

--- LeetCode/#21/src/main.cpp
+++ LeetCode/#21/src/main.cpp
@@ -1,7 +1,7 @@
 //#21.合并两个有序链表
+//传进来的链表是没有头结点的!!!!!!!!!!!!!!!!!!
 
 #include <iostream>
-#include "List.h"
 using namespace std;
 
 struct ListNode {
@@ -13,13 +13,66 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
+
+        ListNode *p1 = l1, *p2 = l2;        
+        ListNode *pHead = new ListNode(-1);
+        ListNode *p = pHead;
+
+        while (p1 != NULL || p2 != NULL)
+        {
+            ListNode *new_node;
+            if (p1 != NULL && p2 != NULL)
+            {
+                new_node = new ListNode(p1->val <= p2->val ? 
+                                        p1->val : p2->val);
+                p1->val <= p2->val ? p1=p1->next : p2=p2->next;                
+            }
+            else if (p1 != NULL)
+            {
+                new_node = new ListNode(p1->val);
+                p1 = p1->next;
+            }
+            else//p2 != NULL
+            {
+                new_node = new ListNode(p2->val);
+                p2 = p2->next;
+            }
+            p->next = new_node;
+            p = p->next;
+        }
+        return pHead->next;
     }
 };
 
+void printList(ListNode* p)
+{
+    while (p != NULL)
+    {
+        printf("[%d]->",p->val);
+        p = p->next;
+    }
+    printf("NULL\n");
+
+}
+
 int main()
 {
+    //leetcode中链表默认没有头结点
+    ListNode *n1 = new ListNode(1);
+    ListNode *n2 = new ListNode(3);
+    ListNode *n3 = new ListNode(5);
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = NULL;
+
+    ListNode *n4 = new ListNode(2);
+    ListNode *n5 = new ListNode(4);
+    ListNode *n6 = new ListNode(6);
+    n4->next = n5;
+    n5->next = n6;
+    n6->next = NULL;
+
     Solution s;
-    
+    printList(s.mergeTwoLists(n1, n4));
     
 }

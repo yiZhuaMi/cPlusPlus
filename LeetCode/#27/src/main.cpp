0fb96b18c32e1bcf0bc0d1fa1a4f32c4e1d53272
yiZhuaMi <ttmarff@163.com>
2020-02-27 22:09:59 +0800

    complete #27

---
+++ LeetCode/#27/src/main.cpp
@@ -0,0 +1,25 @@
+//#26.删除排序数组中的重复项
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // j指向最后一个!=val的数
+        // i遍历每一个数
+        int j = -1;
+        for (size_t i = 0; i < nums.size(); i++)
+            if (nums[i] != val)
+                nums[++j] = nums[i];
+        return j+1;
+    }
+};
+
+int main()
+{
+    vector<int> num = {3,2,2,3};
+    Solution s;
+    printf("%d\n",s.removeElement(num,3));
+}

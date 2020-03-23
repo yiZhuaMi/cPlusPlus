4bab89c01ebfb7fbf8bd6b6185a0f9e366da2de9
yiZhuaMi <ttmarff@163.com>
2020-03-23 15:25:46 +0800

    update

---
+++ LeetCode/Offer/#4/src/main.cpp
@@ -0,0 +1,19 @@
+//#面试题4 二维数组中的查找
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool Find(int target, vector<vector<int>> array) {
+        
+    }
+};
+
+int main()
+{
+    vector<vector<int>> array = {{1,2,3},{4,5,6},{7,8,9}};
+    Solution s;
+    printf("%b\n",s.Find(7,array));
+}
